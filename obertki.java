00eb3b77f28ff9c37541fd1d47704d5df7e86c7f
Katherine_20 <mob1911494@gmail.com>
2022-09-13 13:27:17 +0300

    added obertki peremennih

---
+++ obertki.java
@@ -0,0 +1,15 @@
+package java_lec1;
+/* Классы обертки
+    примитив - класс обертка (в проге инт - потом значение)
+ */ 
+
+public class obertki {
+    public static void main(String[] args) {
+        int i = 123_12_67; // other way of writing
+        System.out.println(Integer.MIN_VALUE); // больший функционал
+
+        String s = "qwerty";
+        s.charAt(1); // numeration from 0
+    }   
+    
+}
